a88f01a9fc746d71bf02bca2fca9c60094f75ca2
蔡明师 <i@unoiou.com>
2019-04-08 14:14:21 +0800

    add os

---
+++ os/precache-manifest.2635909182478c7456d5c3e0f3f0b70e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3445d2f6e1ba1f56a793",
+    "url": "/os/static/css/main.c0649685.chunk.css"
+  },
+  {
+    "revision": "3445d2f6e1ba1f56a793",
+    "url": "/os/static/js/main.2ebedd11.chunk.js"
+  },
+  {
+    "revision": "e70ed260f2689ca34c5b",
+    "url": "/os/static/js/runtime~main.0affaf03.js"
+  },
+  {
+    "revision": "07cfcbb0ab420989b055",
+    "url": "/os/static/js/2.88a5b296.chunk.js"
+  },
+  {
+    "revision": "7eae8478e89dc72334c36476763fe220",
+    "url": "/os/index.html"
+  }
+];
